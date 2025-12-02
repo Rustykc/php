adb9b15618f92d0e75d37645b5f561a20c7fc1d5
Rustykc <samsonovalera313@gmail.com>
2025-12-02 06:40:59 +0300

    vd

--- index.php
+++ index.php
@@ -1,14 +1,103 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Лабораторные работы</title>
+    <style>
+        body {
+            font-family: Arial;
+            margin: 20px;
+            background-color: rgba(241, 123, 252, 0.38);
+        }
+
+        a:link {
+            color: #520052ff;
+        }
+
+        a:visited {
+            color: #5f030f;
+        }
+
+        a:hover {
+            color: #048a9cff;
+            text-decoration: underline;
+        }
+
+        a:active {
+            color: #078686;
+        }
+
+        hr {
+            border-color: #ff6347;
+        }
+    </style>
 </head>
+
 <body>
-    <?php
-    echo 'Привет!';
-    ?>
+    <h1>Лабораторные работы:</h1>
+
+    <hr>
+
+    <h2>Лабораторная работа 1:</h2>
+    <ul>
+        <li><a href="lab1/1_1.php">Номер 1</a></li>
+        <li><a href="lab1/1_2.php">Номер 2</a></li>
+        <li><a href="lab1/1_3.php">Номер 3</a></li>
+        <li><a href="lab1/1_4.php">Номер 4</a></li>
+        <li><a href="lab1/1_5.php">Номер 5</a></li>
+    </ul>
+
+    <hr>
+
+    <h2>Лабораторная работа 2:</h2>
+    <ul>
+        <li><a href="lab2/2_1.php">Номер 1</a></li>
+        <li><a href="lab2/2_2.php">Номер 2</a></li>
+        <li><a href="lab2/2_3.php">Номер 3</a></li>
+        <li><a href="lab2/2_4.php">Номер 4</a></li>
+        <li><a href="lab2/2_5.php">Номер 5</a></li>
+        <li><a href="lab2/2_6.php">Номер 6</a></li>
+        <li><a href="lab2/2_7.php">Номер 7</a></li>
+        <li><a href="lab2/2_8.php">Номер 8</a></li>
+    </ul>
+
+    <hr>
+
+    <h2>Лабораторная работа 3:</h2>
+    <ul>
+        <li><a href="lab3/3_1.php">Номер 1</a></li>
+        <li><a href="lab3/3_2.php">Номер 2</a></li>
+        <li><a href="lab3/3_3.php">Номер 3</a></li>
+        <li><a href="lab3/3_4.php">Номер 4</a></li>
+        <li><a href="lab3/site/index.php">Номер 5</a></li>
+    </ul>
+
+    <hr>
+
+    <h2>Лабораторная работа 4:</h2>
+    <ul>
+        <li><a href="lab4/site/index.php">Номер 1</a></li>
+    </ul>
+
+    <hr>
+
+    <h2>Лабораторная работа 5:</h2>
+    <ul>
+        <li><a href="lab5/5_1.php">Номер 1</a></li>
+        <li><a href="lab5/5_2.php">Номер 2</a></li>
+        <li><a href="lab5/5_3.php">Номер 3</a></li>
+    </ul>
+
+    <hr>
+
+    <h2>Лабораторная работа 6:</h2>
+    <ul>
+        <li><a href="lab6/6_1.php">Номер 1</a></li>
+        <li><a href="lab6/session/menu.inc.php">Номер 2</a></li>
+    </ul>
 
 </body>
+
 </html>
