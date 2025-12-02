b51ee7ddecab62391a319767738ae6a8839dca9f
Rustykc <samsonovalera313@gmail.com>
2025-12-02 09:28:30 +0300

    пу

--- lab3/site/calc.php
+++ lab3/site/calc.php
@@ -8,51 +8,78 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <header>
-    <!-- Верхняя часть страницы -->
-    <img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
-    <span class="slogan">приходите к нам учиться</span>
-    <!-- Верхняя часть страницы -->
-  </header>
-  <section>
-    <!-- Заголовок -->
-    <h1>Калькулятор школьника</h1>
-    <!-- Заголовок -->
-    <!-- Область основного контента -->
-    <form action=''>
-      <label>Число 1:</label>
-      <br>
-      <input name='num1' type='text'>
-      <br>
-      <label>Оператор: </label>
-      <br>
-      <input name='operator' type='text'>
-      <br>
-      <label>Число 2: </label>
-      <br>
-      <input name='num2' type='text'>
-      <br>
-      <br>
-      <input type='submit' value='Считать'>
-    </form>
-    <!-- Область основного контента -->
-  </section>
-  <nav>
-    <h2>Навигация по сайту</h2>
-    <!-- Меню -->
-    <ul>
-      <li><a href='index.php'>Домой</a></li>
-      <li><a href='about.php'>О нас</a></li>
-      <li><a href='contact.php'>Контакты</a></li>
-      <li><a href='table.php'>Таблица умножения</a></li>
-      <li><a href='calc.php'>Калькулятор</a></li>
-    </ul>
-    <!-- Меню -->
-  </nav>
-  <footer>
-    <!-- Нижняя часть страницы -->
-    &copy; Супер Мега Веб-мастер, 2000 &ndash; 20xx
-    <!-- Нижняя часть страницы -->
-  </footer>
+
+<header>
+  <img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
+  <span class="slogan">приходите к нам учиться</span>
+</header>
+
+<section>
+  <h1>Калькулятор школьника</h1>
+
+  <form action="">
+    <label>Число 1:</label><br>
+    <input name="num1" type="text"><br>
+
+    <label>Оператор (+ - * /):</label><br>
+    <input name="operator" type="text"><br>
+
+    <label>Число 2:</label><br>
+    <input name="num2" type="text"><br><br>
+
+    <input type="submit" value="Считать">
+  </form>
+
+  <br>
+
+  <?php
+  if (!empty($_GET['num1']) && !empty($_GET['num2']) && !empty($_GET['operator'])) {
+      $a = $_GET['num1'];
+      $b = $_GET['num2'];
+      $op = $_GET['operator'];
+
+      if (!is_numeric($a) || !is_numeric($b)) {
+          echo "<p style='color:red;'>Ошибка: нужно вводить числа.</p>";
+      } else {
+          $a = (float)$a;
+          $b = (float)$b;
+
+          switch ($op) {
+              case '+': $result = $a + $b; break;
+              case '-': $result = $a - $b; break;
+              case '*': $result = $a * $b; break;
+              case '/':
+                if ($b == 0) {
+                    $result = "Ошибка: деление на ноль";
+                } else {
+                    $result = $a / $b;
+                }
+                break;
+              default:
+                $result = "Неизвестный оператор";
+          }
+
+          echo "<h3>Результат: $result</h3>";
+      }
+  }
+  ?>
+
+</section>
+
+<nav>
+  <h2>Навигация по сайту</h2>
+  <ul>
+    <li><a href='index.php'>Домой</a></li>
+    <li><a href='about.php'>О нас</a></li>
+    <li><a href='contact.php'>Контакты</a></li>
+    <li><a href='table.php'>Таблица умножения</a></li>
+    <li><a href='calc.php'>Калькулятор</a></li>
+  </ul>
+</nav>
+
+<footer>
+  &copy; Супер Мега Веб-мастер, 2000 – 20xx
+</footer>
+
 </body>
 </html>
