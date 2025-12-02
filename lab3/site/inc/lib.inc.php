37242f1c1534aee0362f835374546edda55afbde
Rustykc <samsonovalera313@gmail.com>
2025-12-02 09:56:00 +0300

    ауц

--- lab3/site/inc/lib.inc.php
+++ lab3/site/inc/lib.inc.php
@@ -1,21 +1,33 @@
 <?php
 declare(strict_types=1);
 
-function getTable(int $cols, int $rows, string $color): string {
-    $html = "<table>";
+function getTable(int $rows = 5, int $cols = 5, string $color = '#ff0000'): string {
+    $html = '<table>';
+    for ($i = 1; $i <= $rows; $i++) {
+        $html .= '<tr>';
+        for ($j = 1; $j <= $cols; $j++) {
+            $style = '';
+            if ($i === 1) {
+                $style = "background-color: $color;";
+            }
+            
+            if ($j === 1) {
+                $style = "background-color: $color;";
+            }
 
-    for ($r = 1; $r <= $rows; $r++) {
-        $html .= "<tr>";
-        for ($c = 1; $c <= $cols; $c++) {
-            $value = $r * $c;
-            $html .= "<td style='border-color: $color;'>$value</td>";
+            if ($i === 1 && $j === 1) {
+                $style = "background-color: $color;";
+            }
+
+            $html .= "<td style='$style'>" . ($i * $j) . "</td>";
         }
-        $html .= "</tr>";
+        $html .= '</tr>';
     }
-
-    $html .= "</table>";
+    $html .= '</table>';
     return $html;
 }
+
+
 
 function getMenu(array $menu): string {
     $html = "<ul>";
