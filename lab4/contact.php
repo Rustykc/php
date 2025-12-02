b59525663d19bc285d07474b01eb61c82aa88063
Rustykc <samsonovalera313@gmail.com>
2025-12-02 10:44:26 +0300

    y54

--- lab4/contact.php
+++ lab4/contact.php
@@ -1,66 +1,19 @@
 
-<?php
-$result = null;
-$num1 = '';
-$num2 = '';
-$operator = '+';
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $num1 = filter_input(INPUT_POST, 'num1', FILTER_VALIDATE_FLOAT);
-    $num2 = filter_input(INPUT_POST, 'num2', FILTER_VALIDATE_FLOAT);
-    $operator = filter_input(INPUT_POST, 'operator', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if ($num1 === false || $num2 === false)
-        $result = 'Ошибка: введите корректные числа!';
-    else {
-        switch ($operator) {
-            case '+':
-                $result = $num1 + $num2;
-                break;
-            case '-':
-                $result = $num1 - $num2;
-                break;
-            case '*':
-                $result = $num1 * $num2;
-                break;
-            case '/':
-                if ($num2 == 0)
-                    $result = 'делить на ноль нельзя!';
-                else
-                    $result = $num1 / $num2;
-                break;
-            default:
-                $result = 'Ошибка: некорректный оператор!';
-        }
-    }
-}
-?>
-
-
-<?php
-    if ($result !== null)
-        echo "Результат: $result";
-?>
-
-<form method="post">
-
-    <p><label for="num1">Число 1</label><br>
-        <input type="text" name="num1" id="num1" value="<?= htmlspecialchars($num1) ?>" required>
-    </p>
-
-    <p><label for="operator">Оператор</label><br>
-        <select name="operator" id="operator">
-            <option value="+" <?= $operator == '+' ? 'selected' : '' ?>>+</option>
-            <option value="-" <?= $operator == '-' ? 'selected' : '' ?>>-</option>
-            <option value="*" <?= $operator == '*' ? 'selected' : '' ?>>*</option>
-            <option value="/" <?= $operator == '/' ? 'selected' : '' ?>>/</option>
-        </select>
-    </p>
-
-    <p><label for="num2">Число 2</label><br>
-        <input type="text" name="num2" id="num2" value="<?= htmlspecialchars($num2) ?>" required>
-    </p>
-
-    <button type="submit">Считать!</button>
-
-</form>
+    <!-- Область основного контента -->
+    <h3>Адрес</h3>
+    <address>123456 Москва, Малый Американский переулок 21</address>
+    <h3>Задайте вопрос</h3>
+    <form action='' method='post'>
+      <label>Тема письма: </label>
+      <br>
+      <input name='subject' type='text' size="50">
+      <br>
+      <label>Содержание: </label>
+      <br>
+      <textarea name='body' cols="50" rows="10"></textarea>
+      <br>
+      <br>
+      <input type='submit' value='Отправить'>
+    </form>
+    <!-- Область основного контента -->
+  
